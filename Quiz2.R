05515ef490cc9e6c48ffb2df877b9e70f8e0eefa
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-07-12 10:00:31 -0500

    quiz2

--- Quiz2.R
+++ Quiz2.R
@@ -35,4 +35,51 @@ datasharing$created_at # "2013-11-07T13:25:07Z"
 
 # 2
 
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, destfile = "./data/america.csv", method = "curl")
+
+acs <- read.csv("./data/america.csv")
+str(acs)
+
+acs$pwgtp1
+
+install.packages("sqldf")
+library(sqldf)
+
+
+df <- sqldf("select pwgtp1 from acs where AGEP < 50")
+
+unique(acs$AGEP)
+
+
+# 3
+
+df <- sqldf("select distinct AGEP from acs")
+
+# 4
+
+
+con <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlCode <- readLines(con)
+class(htmlCode)
+close(con) # close the connection 
+
+nchar(c(htmlCode[10], htmlCode[20], htmlCode[30], htmlCode[100]))
+
+# 5
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+download.file(fileUrl, destfile = "./data/cpc.for", method = "curl")
+
+dffr4<- read.fwf("./data/cpc.for", widths=c(-27,5), skip =4)
+head(dffr4)
+c4 <- sum(dffr4)
+c4
+
+
+
+
+
+
+
 
