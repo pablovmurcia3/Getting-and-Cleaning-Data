107b18de4a800a485e7eaf5b9fa72e20822d6f90
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-07-19 18:49:20 -0500

    W4

--- W4.R
+++ W4.R
@@ -188,7 +188,184 @@ grep("^([Nn]orth|Cold)",cameraData$intersection, value=TRUE)
 
 grep("(.*)",cameraData$intersection, value=TRUE) # repetition of any number of charactera
 
-grep("[0-9]+",cameraData$intersection, value=TRUE)
+grep("[A]+(.*)[0-9]+",cameraData$intersection, value=TRUE)
 
 grep("&(.*)Lane",cameraData$intersection, value=TRUE) # & followed by x characters
 # and then Lane
+
+# { and } are referred to as interval quantifiers; the let us specify the 
+# minimum and maximum number of matches of an expression
+
+# Interval quantifiers of the {n, m} form specify n, a min and m, a max 
+# number of matches.
+
+
+# m,n means at least m but not more than n matches 
+# m means exactly m matches
+# m, means at least m matches
+
+[Bb]ush( +[^ ]+ +){1,5} debate
+
+grep("Cold ([^ ]+ +){1,5}Hillen",cameraData$intersection, value=TRUE) 
+
+x <- c("Bush has historically won all major debates he's done.",
+       "in my view, Bush doesn't need these debates..",
+       "Felix, I don't disagree that Bush was poorly prepared for the debate",
+       "Keep repeating that Bush smirked and scowled during the debate",
+       "Bush one word three words five six debate",
+       "Bush debates Kerry", 
+       "Bush doesn't debate anymore",
+       "Bush onespace  twospace  twospace  debate")
+
+grep("[Bb]ush ([^ ]+ +){1,5}debate", x, value = TRUE) # good
+# Bush or bush followed by 1-5 words, followed by debate
+
+grep("[Bb]ush( +[^ ]+ +){1,5} debate", x, value = TRUE) # bad
+
+# In most implementations of regular expressions, the parentheses not only limit 
+# the scope of alternatives divided by a “|”, but also can be used to “remember” 
+# text matched by the subexpression enclosed We refer to the matched text with \1, \2, etc.
+
+x <- c("el tonto tonto jeff")
+grep("+([a-zA-Z]+) +\1 +", x, value = TRUE) # bad
+
+# The * is “greedy” so it always matches the _longest_ possible string that 
+# satisfies the regular expression. So
+
+grep("^G(.*)i", cameraData$intersection, value = TRUE) 
+
+grep("^G(.*?)i", cameraData$intersection, value = TRUE) # bad
+
+# Summary
+
+# Regular expressions are used in many different languages; not unique to R.
+# Regular expressions are composed of literals and metacharacters that represent sets or classes of characters/words
+# Text processing via regular expressions is a very powerful way to extract data from “unfriendly” sources (not all data comes as a CSV file)
+# Used with the functions `grep`,`grepl`,`sub`,`gsub` and others that involve searching for text strings
+# (Thanks to Mark Hansen for some material in this lecture.)
+
+################################################################################
+
+# Working with dates
+
+# Starting simple
+
+d1 <- date()
+d1
+class(d1)
+
+# Date class
+        
+d2 <- Sys.Date()
+d2
+class(d2)
+
+# Formatting dates
+
+# `%d` = day as number (0-31), `%a` = abbreviated weekday,`%A` = unabbreviated 
+# weekday, `%m` = month (00-12), `%b` = abbreviated month,
+# %B` = unabbrevidated month, `%y` = 2 digit year, `%Y` = four digit year
+
+format(d2,"%a %b %d")
+format(d2,"%d %m %Y")
+
+# Creating dates
+
+x <- c("11jan1960", "12jan1960", "30mar1960", "30jul1960")
+z <- as.Date(x, "%d%b%Y")
+z
+
+z[1] - z[2]
+as.numeric(z[1]-z[2])
+
+t<-dmy(x) # GOod!!!!!!!!
+class(t)
+
+# Converting to Julian 
+
+weekdays(d2)
+months(d2)
+julian(d2)
+
+# Lubridate 
+        
+
+install.packages("lubridate")
+library(lubridate)
+
+ymd("20140108")
+
+mdy("08/04/2013")
+
+dmy("03-04-2013")
+
+# Dealing with times
+
+t1 <- ymd_hms("2011-08-03 10:15:03")
+class(t1)
+t2 <- ymd_hms("2011-08-03 10:15:03",tz="Pacific/Auckland")
+class(t2)
+
+?Sys.timezone
+
+
+# Some functions have slightly different syntax
+
+x <- dmy(c("1jan2013", "2jan2013", "31mar2013", "30jul2013"))
+wday(x[1])
+wday(x[1],label=TRUE)
+
+################################################################################
+
+# Open Government Sites
+
+#* United Nations [http://data.un.org/](http://data.un.org/)
+#* U.S. [http://www.data.gov/](http://www.data.gov/)
+#* [List of cities/states with open data](http://simplystatistics.org/2012/01/02/list-of-cities-states-with-open-data-help-me-find/)
+#* United Kingdom [http://data.gov.uk/](http://data.gov.uk/)
+#* France [http://www.data.gouv.fr/](http://www.data.gouv.fr/)
+#* Ghana [http://data.gov.gh/](http://data.gov.gh/)
+#* Australia [http://data.gov.au/](http://data.gov.au/)
+#* Germany [https://www.govdata.de/](https://www.govdata.de/) 
+#* Hong Kong [http://www.gov.hk/en/theme/psi/datasets/](http://www.gov.hk/en/theme/psi/datasets/)
+#* Japan [http://www.data.go.jp/](http://www.data.go.jp/)
+#* Many more [http://www.data.gov/opendatasites](http://www.data.gov/opendatasites)
+
+# Gapminder
+        
+
+# http://www.gapminder.org/](http://www.gapminder.org/
+
+
+# Survey data from the United States
+        
+# Infochimps Marketplace
+        
+# Kaggle
+        
+# Collections by data scientists
+        
+# * Hilary Mason http://bitly.com/bundles/hmason/1
+# * Peter Skomoroch https://delicious.com/pskomoroch/dataset
+# * Jeff Hammerbacher http://www.quora.com/Jeff-Hammerbacher/Introduction-to-Data-Science-Data-Sets
+# * Gregory Piatetsky-Shapiro http://www.kdnuggets.com/gps.html
+# * [http://blog.mortardata.com/post/67652898761/6-dataset-lists-curated-by-data-scientists](http://blog.mortardata.com/post/67652898761/6-dataset-lists-curated-by-data-scientists)
+
+# More specialized collections
+
+#* [Stanford Large Network Data](http://snap.stanford.edu/data/)
+#* [UCI Machine Learning](http://archive.ics.uci.edu/ml/)
+#* [KDD Nugets Datasets](http://www.kdnuggets.com/datasets/index.html)
+#* [CMU Statlib](http://lib.stat.cmu.edu/datasets/)
+#* [Gene expression omnibus](http://www.ncbi.nlm.nih.gov/geo/)
+#* [ArXiv Data](http://arxiv.org/help/bulk_data)
+#* [Public Data Sets on Amazon Web Services](http://aws.amazon.com/publicdatasets/)
+
+# Some API's with R interfaces
+
+#* [twitter](https://dev.twitter.com/) and [twitteR](http://cran.r-project.org/web/packages/twitteR/index.html) package
+#* [figshare](http://api.figshare.com/docs/intro.html) and [rfigshare](http://cran.r-project.org/web/packages/rfigshare/index.html)
+#* [PLoS](http://api.plos.org/) and [rplos](http://cran.r-project.org/web/packages/rplos/rplos.pdf)
+#* [rOpenSci](http://ropensci.org/packages/index.html)
+#* [Facebook](https://developers.facebook.com/) and [RFacebook](http://cran.r-project.org/web/packages/Rfacebook/)
+#* [Google maps](https://developers.google.com/maps/) and [RGoogleMaps](http://cran.r-project.org/web/packages/RgoogleMaps/index.html)
